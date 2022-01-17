560b1230a1bff9744debea2689c029f2057bf79c
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-01-17 11:16:49 -0700

    playing around with async functions

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
-//document.body.style.backgroundColor = "red";
-//test me
+async function test(a,b) {
+    console.log('calling')
+    let num = await addNumber(a,b);
+    const result = num * 2
+    console.log(result);
+    num = await addNumber(result, b)
+    console.log(num)
+}
+
+async function addNumber(a,b) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(a + b)
+        },2000)
+    })
+}
+
+test(2,4)
